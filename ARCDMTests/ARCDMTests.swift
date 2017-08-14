105301e163afdfd3cd18314be39df05413e3ea3b
Matthew Cheetham <matt@3sidedcube.com>
2017-08-14 12:15:49 +0100

    Initial commit

---
+++ ARCDMTests/ARCDMTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARCDMTests.swift
+//  ARCDMTests
+//
+//  Created by Matthew Cheetham on 14/08/2017.
+//  Copyright © 2017 3 SIDED CUBE. All rights reserved.
+//
+
+import XCTest
+@testable import ARCDM
+
+class ARCDMTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
